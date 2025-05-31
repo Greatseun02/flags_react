7a2f311b4220d710a61a4b4945907d53c2edb570
greatseun02 <goodnews.adewole9@gmail.com>
2025-05-31 02:42:08 +0100

    addded more file structure

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Input } from './Input'
 import { Countries } from './Countries'
+import { Filter } from './Filter'
 
 export const Body = () => {
   return (
     <div>
       <Input/>
+      <Filter/>
       <Countries/>
     </div>
   )
--- src/Components/Countries.tsx
+++ src/Components/Countries.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Card } from './Card'
 
 export const Countries = () => {
   return (
-    <div>Countries</div>
+    <div>
+        <Card/>
+    </div>
   )
 }
